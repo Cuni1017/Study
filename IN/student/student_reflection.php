3a8da0762c66b26aea62ffa3d1543835ef02d01e
Cuni <ahet1017@gmail.com>
2022-05-30 22:09:58 +0800

    STU_reflection

--- IN/student/student_reflection.php
+++ IN/student/student_reflection.php
@@ -10,9 +10,26 @@
         <div id="content">
             <h1>瀏覽實習心得</h1>
 
+            <div id="reflectionBox">
+
+                <div class="reflection">
+                    <div class="reflection-row">
+                        <div class="reflection-img"><img src="../../image/心得.png"> </div>
+                        <div class="reflection-text">
+                            <div class="reflection-title">在明偉有限公司實習後，我學到了...</div>
+                            <div class="reflection-content">內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容內容</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div> <!-- content -->
     </div> <!-- wrap -->
 
+    <script>
+        var oReflection_content = document.getElementsByClassName("reflection-content")[0]
+        console.log(oReflection_content)
+        
+    </script>
 </body>
 
 </html>
